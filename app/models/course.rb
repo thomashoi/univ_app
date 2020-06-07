af1997ca6bceda64b63bc1e18debd59934ba77c6
Thomas Hoi <thomashoi2005@gmail.com>
2020-06-07 22:19:27 +0800

    Added many to many association

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ApplicationRecord
   validates :short_name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 3, maximum: 50 }
+  has_many :student_courses
+  has_many :students, through: :student_courses
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(name: 'Sharon', email: 'sharon@hot.com', password: 'password')
+Student.create(name: 'Marcus', email: 'marcus@hot.com', password: 'password')
+Student.create(name: 'Faith', email: 'faith@hot.com', password: 'password')
+Student.create(name: 'Sarah', email: 'sarah@hot.com', password: 'password')
+Course.create(short_name: "CS111", name: "Intro to Computer Science", description: "An in-depeth study into computer science")
+Course.create(short_name: "CS112", name: "Intro to Computer Science 2", description: "An in-depeth study into computer science 2")
+Course.create(short_name: "CS208", name: "Intermed Computer Science", description: "Intermediate study into Computer Science")
